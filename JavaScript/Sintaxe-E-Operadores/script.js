9aeadf00bdd0dbcf39aa642cc14885bbf85cb9f5
Romulo <romulomesquita.06@gmail.com>
2022-03-24 00:09:07 -0300

    Organizando aplicação

---
+++ JavaScript/Sintaxe-E-Operadores/script.js
@@ -0,0 +1,7 @@
+// Criar uma função que recaba dois numeros como parametros
+// coferir se os numeros são iguais
+//conferir se a soma dos numeros é maior que 10 ou menor que 20
+// retornar uma string dizendo "Os números num1 e num2 não/são iguais. Sua soma é maio/menor que 10 e maior/menor que 20"
+
+// EX input 1, 2
+// output Os números 1 e 2 não iguais. Sua soma é 3 que menor que 10 e menor que 20.
